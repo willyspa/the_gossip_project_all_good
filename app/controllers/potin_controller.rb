e34ad6abf16a5164eef1aac8d6bb6d53ee2fb5a8
Yacinth <fevriercjjc@gmail.com>
2019-02-04 20:46:39 +0100

    all gossip on dynamic page

---
+++ app/controllers/potin_controller.rb
@@ -0,0 +1,14 @@
+class PotinController < ApplicationController
+  def show
+    puts "$" * 60
+    puts params[:id_gossip]
+    puts "$" * 60
+    id_params = params[:id_gossip].to_i
+    @array_gossip = Gossip.all
+    @gossip = @array_gossip[id_params-1]
+    #@id_gossip = params[:id_gossip]
+    # @array_gossip = Gossip.all
+    # @id_gossip = Gossip.find(params[:id_gossip])
+    # @where_gossip = Gossip.where(id: params[:id_gossip])
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+  get '/', to: 'welcome#show'
   get 'welcome', to: 'welcome#show'
   get 'welcome/show', to: 'welcome#show'
   get 'static_pages/contact'
   get 'static_pages/team'
   get 'welcome/:first_name', to: 'welcome#show'
+  get 'potin/:id_gossip', to: 'potin#show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
