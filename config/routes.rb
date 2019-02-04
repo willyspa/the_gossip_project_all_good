1a08c6bb72955b996b68cc0805714d5b59553c5c
Yacinth <fevriercjjc@gmail.com>
2019-02-04 16:33:45 +0100

    ajout team et contact

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  get 'static_pages/contact'
+  get 'static_pages/team'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
