eb6882147450282d58df05673528169888b8d53a
Yacinth <fevriercjjc@gmail.com>
2019-02-04 18:07:50 +0100

    welcome

---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,12 @@
+class WelcomeController < ApplicationController
+  def show
+    puts "$" * 60
+    puts "Voici le message de l'URL :"
+    puts  params[:first_name]
+    puts "$" * 60
+    @first_name = params[:first_name]
+    @author = User.first.first_name
+    @title = Gossip.first.title
+    @array_first_name = User.all
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+  get 'welcome', to: 'welcome#show'
+  get 'welcome/show', to: 'welcome#show'
   get 'static_pages/contact'
   get 'static_pages/team'
+  get 'welcome/:first_name', to: 'welcome#show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
