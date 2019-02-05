17e0f9a5e1a04cf2ebd9fc4b5b7b943b314e412c
willyspa <armed95@hotmail.fr>
2019-02-05 20:50:07 +0100

    changement de route retour au route initial

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,17 @@
 Rails.application.routes.draw do
-  #get '/', to: 'welcome#show'
-  #get 'welcome', to: 'welcome#show'
-  #get 'welcome/show', to: 'welcome#show'
-  #get 'static_pages/contact'
-  #get 'static_pages/team'
-  #get 'welcome/:first_name', to: 'welcome#show'
-  #get 'potin/:id_gossip', to: 'potin#show'
-  #get 'potin/', to: 'potin#show'
-  #end  get 'user/:id_user', to: 'user#show'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/', to: 'welcome#show'
+  get 'welcome', to: 'welcome#show'
+  get 'welcome/show', to: 'welcome#show'
+  get 'static_pages/contact'
+  get 'static_pages/team'
+  get 'welcome/:first_name', to: 'welcome#show'
+  get 'potin/:id_gossip', to: 'potin#show'
+  get 'potin/', to: 'potin#show'
+  end  get 'user/:id_user', to: 'user#show'
+  #For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-    resources :potins do
-      resources :comments, only: [:new, :create, :index]
-    end
+  #  resources :potins do
+    #  resources :comments, only: [:new, :create, :index]
+    #end
 
 end
