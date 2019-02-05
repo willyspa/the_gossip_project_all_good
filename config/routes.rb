c6a8c737405f88d86828e2fe7a82c0f623d43ecc
willyspa <armed95@hotmail.fr>
2019-02-05 20:51:48 +0100

    correction erreur route

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   get 'welcome/:first_name', to: 'welcome#show'
   get 'potin/:id_gossip', to: 'potin#show'
   get 'potin/', to: 'potin#show'
-  end  get 'user/:id_user', to: 'user#show'
+  #end  get 'user/:id_user', to: 'user#show'
   #For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   #  resources :potins do
